7728cfe5c9899f74dcde1f46b92b078892be78fe
Bhavadharani7781 <bhavadharani7781@gmail.com>
2026-07-03 23:44:24 +0530

    Create main.tf

---
+++ terraform-workspcae-day-3/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  instance_name = "${terraform.workspace}-instance"
+  instance_type = var.instance_type
+  ami           = var.ami
+}
